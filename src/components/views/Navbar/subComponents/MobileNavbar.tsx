e354cf69d032dfbe93199a4a1fef5f7eb2306f49
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-07 23:14:20 +0500

    Update MobileNavbar.tsx

--- src/components/views/Navbar/subComponents/MobileNavbar.tsx
+++ src/components/views/Navbar/subComponents/MobileNavbar.tsx
@@ -1,17 +1,19 @@
 import { NavbarArray, NavbarItemType } from "@/components/utils/NavbarArrayAndTypes"
 import Link from "next/link"
+import { HiOutlineChevronDown } from "react-icons/hi"
 
 const MobileNavbar = () => {
     
     return (
         <div>
-            <div className='w-full px-6 py4 bg-gray-100'>
+            <div className='w-full px-6 py-4 bg-gray-100'>
                 {
                     NavbarArray.map((item: NavbarItemType, index: number) => (
-                        <li key={index} className="py-2 px-3 hover:bg-purple-600 rounded-md duration-300">
+                        <li key={index} className="py-2 px-3 hover:bg-purple-600 rounded-md duration-300 list-none flex justify-between">
                             <Link href={item.href}>
                                 {item.label}
                             </Link>
+                            {item.isDropDown ? <HiOutlineChevronDown className="mt-1 -rotate-180 group-hover:rotate-0 duration-300" size={15} /> : ""}
                           
                         </li>
                     ))
