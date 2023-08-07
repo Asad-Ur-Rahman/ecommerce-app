3f603aa939cf2574894c73ee8bd2e7876de462de
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-07 22:02:32 +0500

    Update MobileNavbar.tsx

--- src/components/views/Navbar/subComponents/MobileNavbar.tsx
+++ src/components/views/Navbar/subComponents/MobileNavbar.tsx
@@ -1,9 +1,22 @@
+import { NavbarArray, NavbarItemType } from "@/components/utils/NavbarArrayAndTypes"
+import Link from "next/link"
 
 const MobileNavbar = () => {
     
     return (
         <div>
-            <div className='w-full px-6 py4 bg-gray-100'>hi</div>
+            <div className='w-full px-6 py4 bg-gray-100'>
+                {
+                    NavbarArray.map((item: NavbarItemType, index: number) => (
+                        <li key={index} className="py-2 px-3 hover:bg-purple-600 rounded-md duration-300">
+                            <Link href={item.href}>
+                                {item.label}
+                            </Link>
+                          
+                        </li>
+                    ))
+                }
+            </div>
         </div>
     )
 }
