916420f488e7d44880ff7f403c64cbb2411088e3
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-08 00:20:33 +0500

    Update MobileNavbar.tsx

--- src/components/views/Navbar/subComponents/MobileNavbar.tsx
+++ src/components/views/Navbar/subComponents/MobileNavbar.tsx
@@ -1,15 +1,17 @@
 import { NavbarArray, NavbarItemType } from "@/components/utils/NavbarArrayAndTypes"
 import Link from "next/link"
+import { useState } from "react"
 import { HiOutlineChevronDown } from "react-icons/hi"
 
 const MobileNavbar = () => {
+    const [isExpended, setisExpended] = useState(false)
 
     return (
         <div>
             <div className='w-full px-6 py-4 bg-gray-100'>
                 {
                     NavbarArray.map((item: NavbarItemType, index: number) => (
-                        <li key={index} className={`h-12 flex items-center py-2 px-3 hover:bg-purple-600 rounded-md duration-300 list-none`}>
+                        <li key={index} className={`h-12 items-center py-2 px-3 hover:bg-purple-600 rounded-md duration-300 list-none`}>
                             <div className="flex justify-between">
 
                                 <Link href={item.href}>
@@ -19,6 +21,7 @@ const MobileNavbar = () => {
                             </div>
                             <div>
                                 {
+                                    isExpended &&
                                     item.dropDownData?.map((subItem: NavbarItemType, index: number) => (
                                         <div>{subItem.label}</div>
                                     ))
