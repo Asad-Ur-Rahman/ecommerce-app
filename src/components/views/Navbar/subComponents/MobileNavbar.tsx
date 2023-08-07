db07239a7ec975f0e9c55fcf6ebf02fe87ab8a1e
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-08 00:13:45 +0500

    Update MobileNavbar.tsx

--- src/components/views/Navbar/subComponents/MobileNavbar.tsx
+++ src/components/views/Navbar/subComponents/MobileNavbar.tsx
@@ -3,18 +3,28 @@ import Link from "next/link"
 import { HiOutlineChevronDown } from "react-icons/hi"
 
 const MobileNavbar = () => {
-    
+
     return (
         <div>
             <div className='w-full px-6 py-4 bg-gray-100'>
                 {
                     NavbarArray.map((item: NavbarItemType, index: number) => (
-                        <li key={index} className="py-2 px-3 hover:bg-purple-600 rounded-md duration-300 list-none flex justify-between">
-                            <Link href={item.href}>
-                                {item.label}
-                            </Link>
-                            {item.isDropDown ? <HiOutlineChevronDown className="mt-1 -rotate-180 group-hover:rotate-0 duration-300" size={15} /> : ""}
-                          
+                        <li key={index} className={`h-12 flex items-center py-2 px-3 hover:bg-purple-600 rounded-md duration-300 list-none`}>
+                            <div className="flex justify-between">
+
+                                <Link href={item.href}>
+                                    {item.label}
+                                </Link>
+                                {item.isDropDown ? <HiOutlineChevronDown className="mt-1 -rotate-180 group-hover:rotate-0 duration-300" size={15} /> : ""}
+                            </div>
+                            <div>
+                                {
+                                    item.dropDownData?.map((subItem: NavbarItemType, index: number) => (
+                                        <div>{subItem.label}</div>
+                                    ))
+                                }
+                            </div>
+
                         </li>
                     ))
                 }
