679d1bc268ebfb0e17b49e5e224639d3dac4fd77
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-08 05:34:51 +0500

    Update index.tsx

--- src/components/views/Hero/index.tsx
+++ src/components/views/Hero/index.tsx
@@ -6,11 +6,20 @@ const Hero = () => {
     return (
         <div className="flex justify-between items-center">
             {/* right side */}
-            <div></div>
+            <div>
+                <button className="rounded-md bg-primaryWhite text-purple-700">Sale 70%</button>
+
+                <button className="rounded-sm ring-1 ring-slate-800 bg-gray-900 py-3 px-5 text-white font-semibold">
+                    <p className="text-primaryWhite whitespace-pre-line">
+
+                    Start shopping
+                    </p>
+                </button>
+            </div>
 
             {/* left side */}
-            <div className="bg-gray-200 rounded-full">
-                <Image src={heroGirlimg} alt="heroimg"/>
+            <div className="bg-primaryWhite rounded-full">
+                <Image src={heroGirlimg} alt="heroimg" />
             </div>
         </div>
     )
