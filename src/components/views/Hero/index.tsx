e365e3a42d644089d68aae5b7c9c697f5942830f
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-08 05:17:09 +0500

    Update index.tsx

--- src/components/views/Hero/index.tsx
+++ src/components/views/Hero/index.tsx
@@ -1,9 +1,19 @@
+import { heroGirlimg } from "@/components/assets"
+import Image from "next/image"
 
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
+    return (
+        <div className="flex justify-between items-center">
+            {/* right side */}
+            <div></div>
+
+            {/* left side */}
+            <div className="bg-gray-200 rounded-full">
+                <Image src={heroGirlimg} alt="heroimg"/>
+            </div>
+        </div>
+    )
 }
 
 export default Hero
