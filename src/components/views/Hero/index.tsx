562a3e8254732504dbe42fcde43fd1137fba3590
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-08 05:51:40 +0500

    Update index.tsx

--- src/components/views/Hero/index.tsx
+++ src/components/views/Hero/index.tsx
@@ -1,18 +1,24 @@
 import { heroGirlimg } from "@/components/assets"
 import Image from "next/image"
+import { BsCart2 } from "react-icons/bs";
 
 
 const Hero = () => {
+    const btnText = "Start \n shopping";
+
     return (
         <div className="flex justify-between items-center">
             {/* right side */}
             <div>
                 <button className="rounded-md bg-primaryWhite text-purple-700">Sale 70%</button>
 
-                <button className="rounded-sm ring-1 ring-slate-800 bg-gray-900 py-3 px-5 text-white font-semibold">
-                    <p className="text-primaryWhite whitespace-pre-line">
+                <h1 className="text-2xl text-gray-200 font-bold">An Industrial Take on Streetwear</h1>
+                <p>Anyone can beat you but no one can beat your outfit as long as you wear Dine outfits.</p>
+                <button className="flex gap-1 rounded-sm text-lg ring-1 ring-slate-800 bg-gray-900 py-3 px-5 text-white font-semibold">
+                    <BsCart2 size={24} />
+                    <p className="text-primaryWhite whitespace-pre leading-4">
 
-                    Start shopping
+                        {btnText}
                     </p>
                 </button>
             </div>
