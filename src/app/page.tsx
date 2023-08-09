5968a54da25813f3ada78fa17e39dec208e58f8c
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-09 18:55:40 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/views/Hero";
+import ProductsTypes from "@/components/views/productTypes";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <ProductsTypes />
     </div>
   )
 }
