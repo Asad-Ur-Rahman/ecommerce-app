c49b52642b89879706dfbae061823e8f64cae445
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-13 16:05:04 +0500

    Update index.tsx

--- src/components/views/productTypes/index.tsx
+++ src/components/views/productTypes/index.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const ProductsTypes = () => {
   return (
-    <div>ProductsTypes</div>
+    <div className='py-16 px-2'>
+      <div className='text-center border'>
+        <div>
+          <p className='text-blue-800'>PROMOTIONS</p>
+        </div>
+        <div>
+          <h3 className='text-3xl text-gray-800 font-semibold'>Our Promotions Events</h3>
+        </div>
+      </div>
+
+      <div></div>
+
+    </div>
+
   )
 }
 
