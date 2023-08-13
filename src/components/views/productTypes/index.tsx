2be29a8baedec6a8e58b12d3c6133572151a07d2
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-13 16:32:56 +0500

    Update index.tsx

--- src/components/views/productTypes/index.tsx
+++ src/components/views/productTypes/index.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 const ProductsTypes = () => {
   return (
     <div className='py-16 px-2'>
-      <div className='text-center border space-y-3'>
+      <div className='text-center space-y-3'>
 
         <p className='text-blue-800 text-sm'>PROMOTIONS</p>
         <h3 className='text-3xl text-gray-800 font-semibold'>Our Promotions Events</h3>
 
       </div>
 
-      <div className=' grid grid-cols-4'>
+      <div className=' grid grid-cols-4 gap-4'>
         <div className='w-full col-span-2 h-28 bg-gray-400'></div>
         <div className='w-full row-span-2 h-full bg-purple-400'></div>
         <div className='w-full row-span-2 h-full bg-gray-800'></div>
