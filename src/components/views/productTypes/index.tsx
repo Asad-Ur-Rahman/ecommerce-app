f9f7dabac777c8bb14b9056d979fa673243b6c04
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-14 16:37:41 +0500

    Update index.tsx

--- src/components/views/productTypes/index.tsx
+++ src/components/views/productTypes/index.tsx
@@ -14,7 +14,7 @@ const ProductsTypes = () => {
 
       <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 px-2 text-gray-800'>
 
-        <div className='w-full flex flex-col items-center justify-between md:flex-row col-span-2 bg-cat1 px-12'>
+        <div className='w-full flex flex-col items-center justify-between md:flex-row col-span-1 md:col-span-2 bg-cat1 px-12'>
           <div className='max-w-[13rem] py-8'>
             <h4 className='text-3xl font-extrabold'>GET UP TO 60%</h4>
             <p className='text-xl'>For the summer season</p>
@@ -24,7 +24,7 @@ const ProductsTypes = () => {
           </div>
         </div>
 
-        <div className='w-full row-span-2 h-full bg-cat3'>
+        <div className='w-full row-span-1 md:row-span-2 flex flex-col md:flex-row items-center h-full bg-cat3'>
           <div className="p-4">
             <p>Flex Sweatshirt</p>
             <p className=" text-lg"> <del>$100.00</del> <strong>$75.00</strong></p>
@@ -34,7 +34,7 @@ const ProductsTypes = () => {
           </div>
         </div>
 
-        <div className='w-full row-span-2 h-full bg-cat4'>
+        <div className='w-full row-span-1 md:row-span-2 flex flex-col md:flex-row items-center h-full bg-cat4'>
           <div className="p-4">
             <p>Flex Push Button Bomber</p>
             <p className=" text-lg"> <del>$225.00</del> <strong>$190.00</strong></p>
@@ -43,7 +43,8 @@ const ProductsTypes = () => {
             <Image width={1000} height={1000} src={event3} alt="sweaters" />
           </div>
         </div>
-        <div className='py-4 w-full col-span-2 bg-cat2 flex flex-col justify-center items-center text-white space-y-3'>
+
+        <div className='py-4 w-full col-auto md:col-span-2 bg-cat2 flex flex-col justify-center items-center text-white space-y-3'>
           <h5 className="font-extrabold text-4xl">GET 30% Off</h5>
           <p>USE PROMO CODE</p>
           <button
