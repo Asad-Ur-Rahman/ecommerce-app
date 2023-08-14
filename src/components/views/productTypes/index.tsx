94837f8a4ccbd944f1b60d2349dab9a5ff9a191d
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-14 16:14:34 +0500

    Update index.tsx

--- src/components/views/productTypes/index.tsx
+++ src/components/views/productTypes/index.tsx
@@ -12,7 +12,7 @@ const ProductsTypes = () => {
 
       </div>
 
-      <div className=' grid grid-cols-4 gap-5 px-2 text-gray-800'>
+      <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 px-2 text-gray-800'>
         <div className='w-full flex col-span-2 bg-cat1 px-12'>
           <div className='max-w-[13rem] py-8'>
             <h4 className='text-3xl font-extrabold'>GET UP TO 60%</h4>
