9608656b310f2e946302fd4d8dc8041759f69c5d
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-16 03:51:28 +0500

    Update route.ts

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,10 +1,19 @@
+import { createClient } from "next-sanity";
 import { NextResponse } from "next/server";
+
+const client = createClient({
+    projectId: `${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}`,
+    dataset: `${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
+    apiVersion: "2022-03-25",
+    useCdn: false
+})
 
 export async function GET() {
     try {
         return NextResponse.json({message: "Hi"})
         
     } catch (error) {
-        return NextResponse.json({message: "failed"})
+        console.log((error as { message: string}).message)
+        return NextResponse.json({"Error": error})
     }
 }
