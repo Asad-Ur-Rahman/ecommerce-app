bee839a357e077dc8478290b2fee59608790c0af
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-16 05:47:55 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 import Hero from "@/components/views/Hero";
 import ProductsTypes from "@/components/views/ProductTypes";
 
+async function fetchAllProductsData() {
+  let res = await fetch("/api/products");
 
-export default function Home() {
+  if (!res.ok) {
+    throw new Error("Failed to fetch")
+  }
+
+  return res.json();
+}
+
+
+export default async function Home() {
+  let response = await fetchAllProductsData();
+  console.log("response :", response)
   return (
     <div>
       <Hero />
