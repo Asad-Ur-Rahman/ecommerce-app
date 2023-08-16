b50eabb8f5747a83cd4c83ce931c36cb627ebbef
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-16 06:37:53 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
+import BASE_PATH_FORAPI from "@/components/shared/BasePath";
 import Hero from "@/components/views/Hero";
 import ProductsTypes from "@/components/views/ProductTypes";
 
 async function fetchAllProductsData() {
-  let res = await fetch("/api/products");
+  // let res = await fetch(`${BASE_PATH_FORAPI}/api/products`);
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch")
-  }
+  // if (!res.ok) {
+  //   throw new Error("Failed to fetch")
+  // }
 
-  return res.json();
+  // return res.json();
+  return {response: "Hi"}
 }
 
 
 export default async function Home() {
-  let response = await fetchAllProductsData();
+  let {response} = await fetchAllProductsData();
   console.log("response :", response)
   return (
     <div>
