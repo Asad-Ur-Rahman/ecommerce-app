22246f38def81b7660dfd5fe7b343e04a84c6f3c
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-20 22:08:33 +0500

    Update Card.tsx

--- src/components/views/Card.tsx
+++ src/components/views/Card.tsx
@@ -1,16 +1,25 @@
 import React, { FC } from 'react'
 import { oneProductType } from '../utils/ProductsDataArrayAndType'
 import Image from 'next/image'
+import imageUrlBuilder from '@sanity/image-url'
+import { client } from '../../../sanity/lib/client';
+
+const builder = imageUrlBuilder(client);
+
+function urlFor(source: any) {
+    return builder.image(source)
+}
 
 const Card: FC<{ singleProductData: oneProductType }> = ({ singleProductData }) => {
-    console.log(singleProductData.image)
-  return (
-    <div className=' border-4'>
-        <div>
-            <Image src='/Logo.webp' alt='' width={500} height={500}/>
+    
+    console.log(urlFor(singleProductData.image[0]).width(500).url())
+    return (
+        <div className=' border-4'>
+            <div>
+                <Image src='/Logo.webp' alt='' width={500} height={500} />
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Card
