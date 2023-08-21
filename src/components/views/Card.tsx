be9bcc36b14fea1334ee538fa5f76c70bf328286
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-21 19:50:36 +0500

    Update Card.tsx

--- src/components/views/Card.tsx
+++ src/components/views/Card.tsx
@@ -3,6 +3,7 @@ import { oneProductType } from '../utils/ProductsDataArrayAndType'
 import Image from 'next/image'
 import imageUrlBuilder from '@sanity/image-url'
 import { client } from '../../../sanity/lib/client';
+import Link from 'next/link';
 
 const builder = imageUrlBuilder(client);
 
@@ -12,18 +13,20 @@ function urlFor(source: any) {
 
 const Card: FC<{ singleProductData: oneProductType }> = ({ singleProductData }) => {
     return (
-        <div className='max-w-sm border-4 min-w-[24rem] space-y-3 hover:scale-110 duration-300'>
-            <div className='relative w-full'>
-                <div className='absolute inset-0 z-10'/>
-                <Image src={urlFor(singleProductData.image[0]).width(1000).height(1000).url()} alt={singleProductData.image[0].alt} width={1000} height={1000} />
+        <Link href={`/catalog/${singleProductData.slug.current}`}>
+            <div className='max-w-sm border-4 min-w-[24rem] space-y-3 hover:scale-110 duration-300'>
+                <div className='relative w-full'>
+                    <div className='absolute inset-0 z-10' />
+                    <Image src={urlFor(singleProductData.image[0]).width(1000).height(1000).url()} alt={singleProductData.image[0].alt} width={1000} height={1000} />
+                </div>
+                <div className='space-y-1 text-gray-600 font-semibold text-lg '>
+                    <h6>{singleProductData.productName}</h6>
+
+                    <p>${singleProductData.price}</p>
+
+                </div>
             </div>
-            <div className='space-y-1 text-gray-600 font-semibold text-lg '>
-                <h6>{singleProductData.productName}</h6>
-                
-                <p>${singleProductData.price}</p>
-                
-            </div>
-        </div>
+        </Link>
     )
 }
 
