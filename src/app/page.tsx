36c9a5a02b1ac75af4b51ea16a65798980539d60
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-21 19:50:49 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import BASE_PATH_FORAPI from "@/components/shared/BasePath";
 import { responseType } from "@/components/utils/ProductsDataArrayAndType";
 import Hero from "@/components/views/Hero";
+import Jewellery from "@/components/views/Jewellery";
 import ProductCarousel from "@/components/views/ProductCarousel";
 import ProductsType from "@/components/views/ProductTypes";
 
@@ -25,6 +26,7 @@ export default async function Home() {
       <Hero />
       <ProductsType />
       <ProductCarousel ProductData={result} />
+      <Jewellery />
     </div>
   )
 }
