31f8be9f24252d623fd1a2d42289c820c38a87d9
Asad-Ur-Rahman <Asad.UrRahman@outlook.com>
2023-08-21 21:10:20 +0500

    Update Card.tsx

--- src/components/views/Card.tsx
+++ src/components/views/Card.tsx
@@ -1,7 +1,7 @@
-import React, { FC } from 'react'
-import { oneProductType } from '../utils/ProductsDataArrayAndType'
 import Image from 'next/image'
 import imageUrlBuilder from '@sanity/image-url'
+import React, { FC } from 'react'
+import { oneProductType } from '../utils/ProductsDataArrayAndType'
 import { client } from '../../../sanity/lib/client';
 import Link from 'next/link';
 
@@ -11,22 +11,21 @@ function urlFor(source: any) {
     return builder.image(source)
 }
 
+
 const Card: FC<{ singleProductData: oneProductType }> = ({ singleProductData }) => {
     return (
-        <Link href={`/catalog/${singleProductData.slug.current}`}>
-            <div className='max-w-sm border-4 min-w-[24rem] space-y-3 hover:scale-110 duration-300'>
-                <div className='relative w-full'>
-                    <div className='absolute inset-0 z-10' />
-                    <Image src={urlFor(singleProductData.image[0]).width(1000).height(1000).url()} alt={singleProductData.image[0].alt} width={1000} height={1000} />
-                </div>
-                <div className='space-y-1 text-gray-600 font-semibold text-lg '>
-                    <h6>{singleProductData.productName}</h6>
-
-                    <p>${singleProductData.price}</p>
-
-                </div>
+        <div className='max-w-sm min-w-[24rem] space-y-3 select-none hover:scale-110 duration-300'>
+            <div className='relative w-full'>
+                <div className='absolute inset-0 z-10' />
+                <Image width={1000} height={1000} src={urlFor(singleProductData.image[0]).width(1000).height(1000).url()} alt={singleProductData.image[0].alt} />
             </div>
-        </Link>
+            <div className='space-y-1 text-gray-600 font-semibold text-lg select-none'>
+                <Link href={`/catalog/${singleProductData.slug.current}`}>
+                    <h6>{singleProductData.productName}</h6>
+                    <p>${singleProductData.price}</p>
+                </Link>
+            </div>
+        </div>
     )
 }
 
